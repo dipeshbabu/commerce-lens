2e71b41e6d49bb68e0cb5369cf29d884e3560bdf
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-04-25 00:48:34 -0400

    Export alert scheduler SDK APIs

--- commercelens/__init__.py
+++ commercelens/__init__.py
@@ -1,5 +1,9 @@
 """CommerceLens public SDK."""
 
+from commercelens.alerts.config import MonitorConfig, MonitorTarget, load_monitor_config, save_example_config
+from commercelens.alerts.delivery import AlertDeliveryReport, DeliveryResult, deliver_alert
+from commercelens.alerts.rules import AlertCondition, AlertDestination, AlertDestinationType, AlertEvent, AlertRule
+from commercelens.alerts.runner import MonitorRunResult, run_monitor_config, run_monitor_config_file
 from commercelens.core.crawler import CatalogCrawlResult, crawl_catalog
 from commercelens.core.monitor import BatchMonitorResult, MonitorResult, monitor_product, monitor_products
 from commercelens.extractors.listing import extract_listing, extract_listing_from_html
@@ -9,21 +13,36 @@ from commercelens.schemas.product import Product, ProductExtractionResult
 from commercelens.storage.price_store import PriceChange, PriceSnapshotStore, ProductSnapshot
 
 __all__ = [
+    "AlertCondition",
+    "AlertDeliveryReport",
+    "AlertDestination",
+    "AlertDestinationType",
+    "AlertEvent",
+    "AlertRule",
     "BatchMonitorResult",
     "CatalogCrawlResult",
+    "DeliveryResult",
     "ListingExtractionResult",
     "ListingProduct",
+    "MonitorConfig",
     "MonitorResult",
+    "MonitorRunResult",
+    "MonitorTarget",
     "PriceChange",
     "PriceSnapshotStore",
     "Product",
     "ProductExtractionResult",
     "ProductSnapshot",
     "crawl_catalog",
+    "deliver_alert",
     "extract_listing",
     "extract_listing_from_html",
     "extract_product",
     "extract_product_from_html",
+    "load_monitor_config",
     "monitor_product",
     "monitor_products",
+    "run_monitor_config",
+    "run_monitor_config_file",
+    "save_example_config",
 ]
