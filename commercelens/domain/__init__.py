e300bd0e13e688ee0bad709115d1e0f3fd871ce7
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-08-26 23:29:17 -0400

    Export commerce domain models

---
+++ commercelens/domain/__init__.py
@@ -0,0 +1,35 @@
+"""Tenant scoped commerce domain models and persistence."""
+
+from commercelens.domain.models import (
+    ChangeEventRecord,
+    DomainIngestResult,
+    MonitorCreate,
+    MonitorRecord,
+    ObservationRecord,
+    OfferCreate,
+    OfferRecord,
+    ProductCreate,
+    ProductMatchCreate,
+    ProductMatchRecord,
+    ProductMatchStatus,
+    ProductRecord,
+    SourceCreate,
+    SourceRecord,
+)
+
+__all__ = [
+    "ChangeEventRecord",
+    "DomainIngestResult",
+    "MonitorCreate",
+    "MonitorRecord",
+    "ObservationRecord",
+    "OfferCreate",
+    "OfferRecord",
+    "ProductCreate",
+    "ProductMatchCreate",
+    "ProductMatchRecord",
+    "ProductMatchStatus",
+    "ProductRecord",
+    "SourceCreate",
+    "SourceRecord",
+]
